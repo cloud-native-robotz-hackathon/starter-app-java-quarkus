17e3fa655ddb5b80097f4dd012f14c7c4414b7ec
Daniel Brintzinger <daniel@nexus-six.de>
2023-09-07 17:08:06 +0200

    updated quarkus version removed spring rest client added devfile added s2i

--- src/main/java/io/openshift/booster/service/RobotEndpoint.java
+++ src/main/java/io/openshift/booster/service/RobotEndpoint.java
@@ -1,18 +1,20 @@
 package io.openshift.booster.service;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
 
 //This class provides a RESTful API with a /run method that can be triggered 
 //through the main app webpage. It is based on Quarkus run time. You don't need an Application to run it 
@@ -24,22 +26,35 @@ public class RobotEndpoint {
     // This contains the uri of the Robot APi that this application is invoking. The
     // value is defined in application.properties.
     // Set it to your specific API
-    @Value("${hub.controller.uri}")
+    @ConfigProperty(name = "hub.controller.uri")
     String hubControllerEndpoint;
 
-    private RestTemplate restTemplate = new RestTemplate();
+    CloseableHttpClient httpclient = HttpClients.createDefault();
 
     // This method checks if the HubController can be reached
-    
+
     // This contains the uri of the Robot APi that this application is invoking. The
     // value is defined in application.properties.
     // Set it to your specific API
     @GET
-    public Object ping() {
+    public Object ping() throws ClientProtocolException, IOException {
 
         System.out.println("Ping method invoked");
-        String response= "Ping method invoked";
-        //String response = restTemplate.getForObject(hubControllerEndpoint, String.class);
+        String response = "Ping method invoked";
+
+        CloseableHttpResponse httpResponse = httpclient.execute(new HttpGet(hubControllerEndpoint));
+
+        try {
+
+            String responseString = new BasicResponseHandler().handleResponse(httpResponse);
+            System.out.println(responseString);
+
+            HttpEntity entity1 = httpResponse.getEntity();
+            EntityUtils.consume(entity1);
+        } finally {
+            httpResponse.close();
+        }
+
         return response;
     }
 
@@ -47,27 +62,51 @@ public class RobotEndpoint {
     // by the main application website
     @POST
     @Path("/run")
-    public Object run() {
+    public Object run() throws ClientProtocolException, IOException {
 
         System.out.println("Run method invoked");
 
         String response = "Run method invoked";
 
         // Example GET invokation of the Robot API
-        // response = restTemplate.getForObject(hubControllerEndpoint +
-        // "/power?user_key=<YOUR USER KEY>", String.class);
+
+        /*
+         * CloseableHttpResponse httpResponse = httpclient.execute(new
+         * HttpGet(hubControllerEndpoint + "/distance"));
+         * 
+         * try {
+         * 
+         * response = new BasicResponseHandler().handleResponse(httpResponse);
+         * System.out.println(response);
+         * 
+         * HttpEntity entity1 = httpResponse.getEntity();
+         * EntityUtils.consume(entity1);
+         * } finally {
+         * httpResponse.close();
+         * }
+         */
 
         // Example POST invokation of the Robot API
-        // MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<String,
-        // String>();
-        // paramMap.add("user_key", "<YOUR USER KEY>");
-        // HttpEntity<MultiValueMap<String, String>> request = new
-        // HttpEntity<MultiValueMap<String, String>>(paramMap,
-        // new LinkedMultiValueMap<String, String>());
-        // response = restTemplate.postForObject(hubControllerEndpoint + "/forward/5",
-        // request, String.class);
 
-        return response;
+        /*
+         * HttpPost httpPost = new HttpPost(hubControllerEndpoint + "/forward/5");
+         * List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+         * nvps.add(new BasicNameValuePair("userKey", "<USERKEY>"));
+         * httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+         * CloseableHttpResponse httpResponse = httpclient.execute(httpPost);
+         * 
+         * try {
+         * response = new BasicResponseHandler().handleResponse(httpResponse);
+         * System.out.println(response);
+         * HttpEntity entity = httpResponse.getEntity();
+         * 
+         * EntityUtils.consume(entity);
+         * } finally {
+         * httpResponse.close();
+         * }
+         * 
+         * return response;
+         */
     }
 
 }
--- src/test/java/io/openshift/booster/service/NativeRobotEndpointIT.java
+++
@@ -1,9 +0,0 @@
-package io.openshift.booster.service;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeRobotEndpointIT extends RobotEndpointTest {
-
-    // Execute the same tests but in native mode.
-}
